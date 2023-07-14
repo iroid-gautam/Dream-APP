486e18d517775b758db1742a978b80926c72db37
DESKTOP-ujwal <ujwal@iroidsolutions.com>
2023-07-14 14:53:55 +0530

    refactor: journal listing add fields

--- src/myJournal/resources/journalListingResource.js
+++ src/myJournal/resources/journalListingResource.js
@@ -1,10 +1,11 @@
 import moment from "moment";
-
+import { baseUrl } from "../../common/constants/constant"
 export default class JournalListingResource {
     constructor(journal) {
         return journal.map(data => ({
             _id: data._id,
             description: data.description,
+            image: data.image !== null ? baseUrl(data.image) : null,
             createdDate: moment(data.createdAt).unix()
         }));
     }
